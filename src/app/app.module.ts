0bd31dc4fa15034fb31bd9735d8039a6f992fdb5
leonardo <leonardomaia@frwk.com>
2022-02-21 11:54:08 -0300

    Rodapé, input component, colappse Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PublicRoutingModule } from './public-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { HomeInputComponent } from './components/home-input/home-input.component';
+import { SharedComponentsModule } from 'src/app/shared/components/shared-components.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeInputComponent],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+    SharedComponentsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
   ]
 })
 export class PublicModule { }
