2601b1766a9d915758c9958dc4c5686fca1f1abc
leonardo <leonardomaia@frwk.com>
2022-02-25 15:23:29 -0300

    carrousel_home, guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginGuard } from './core/guards/login.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'public', pathMatch: 'full'},
   { path: 'public', loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule) },
-  { path: 'private', loadChildren: () => import('./modules/private/private.module').then(m => m.PrivateModule)
-}];
+  { path: 'private', loadChildren: () => import('./modules/private/private.module').then(m => m.PrivateModule), canActivate: [LoginGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/private/components/header/header.component.ts
+++ src/app/modules/private/components/header/header.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  generos = [
+    {id: 28, title: "Filmes de Ação"},
+    {id: 12, title: "Filmes de Aventura"},
+    {id: 16, title: "Animações"},
+    {id: 35, title: "Filmes de Comédia"},
+    {id: 80, title: "Filmes de Crime"},
+    {id: 99, title: "Documentários"},
+    {id: 18, title: "Filmes de Drama"}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
