7dc3ac840940a3bb1dcd11f97d5b1856ec023fe8
leonardo <leonardomaia@frwk.com>
2022-02-25 14:32:14 -0300

    componente_usuario_filmes

--- src/app/modules/private/private.module.ts
+++ src/app/modules/private/private.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrivateRoutingModule } from './private-routing.module';
-import { HeaderComponent } from './header/header.component';
-
 import { CardsSliderComponent } from './components/cards-slider/cards-slider.component';
 import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
-<<<<<<< HEAD
-  declarations: [HeaderComponent],
-=======
-  declarations: [CardsSliderComponent],
->>>>>>> 32b937222a50d284a32b25ab2b725552075942e1
+  declarations: [HeaderComponent, CardsSliderComponent],
   imports: [
     CommonModule,
     PrivateRoutingModule,
