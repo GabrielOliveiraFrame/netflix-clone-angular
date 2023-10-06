e63368b622f701c04278677846ea2a6f51b5a453
Gabriel Oliveira <89689001+techGabrielBr@users.noreply.github.com>
2023-10-06 18:34:24 -0300

    Add prod env for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  API_USER: 'https://json-server-netflix-six.vercel.app/Users',
+  API_KEY: 'f4da42263cee6bb7b4d913ab8fbae012',
+  API_MOVIES_DISCOVER:'https://api.themoviedb.org/3/discover/movie',
+  API_MOVIES:'https://api.themoviedb.org/3/movie',
+  API_GENRES: 'https://api.themoviedb.org/3/genre/movie/list'
 };
